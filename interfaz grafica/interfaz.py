9a6a66a61d21812a8bdbb7099ec859298d0f9c6b
rodrigopazdiaz <43299895+rodrigopazdiaz@users.noreply.github.com>
2019-10-21 12:36:20 -0500

    Create interfaz.py
    interfaz gráfica para el proyecto

---
+++ interfaz grafica/interfaz.py
@@ -0,0 +1,38 @@
+import sys
+from Tkinter import *
+import tkFileDialog
+import tkMessageBox
+from tkFileDialog import askopenfile
+from tkFileDialog import askopenfilename
+
+import os
+
+fasta=""
+lista=""
+window=Tk()
+window.title("lector de fasta")
+
+def callback1():
+    file = askopenfile(mode ='r', filetypes =[('Fasta Files', '*.fasta')]) 
+    if file is not None: 
+        content = file.read()
+	root = Tk()
+	S = Scrollbar(root)
+	T = Text(root, height=20, width=70)
+	S.pack(side=RIGHT, fill=Y)
+	T.pack(side=LEFT, fill=Y)
+	S.config(command=T.yview)
+	T.config(yscrollcommand=S.set) 
+        T.insert(END, content) 
+	root.mainloop()
+def callback2():
+	lista=askopenfilename()
+	print lista
+	return lista
+def instruc():
+   tkMessageBox.showinfo( "instrucciones", "como usar el programa")
+errmsg='error!'
+but_fas=Button(pady=40,padx=200,bg='yellow',text='instrucciones de uso',command=instruc).pack(fill=X)
+but_fas=Button(pady=40,padx=200,bg='green',text='abrir archivo Fasta',command=callback1).pack(fill=X)
+but_lis=Button(pady=40,padx=200,bg='red',text='abrir archivo Lista',command=callback2).pack(fill=X)
+window.mainloop()
